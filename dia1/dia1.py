2e0a2a1eb2bb8c312349314308ba9a98268b6a4f
dancadev <dancadev@gmail.com>
2023-01-04 16:51:29 -0500

    Primer commit

---
+++ dia1/dia1.py
@@ -0,0 +1,56 @@
+print('este es el ejercicio del dia 1 de python')
+suma = 2+2
+print(suma)
+
+diccionario = {
+    'nombre':'David',
+    'Area':'Planeacion',
+    'Cargo':'Soporte tics'
+}
+print(diccionario)
+print(type(diccionario))
+
+una_tupla=('contenido de tupla','otra tupla','y otra tupla mas')
+print(una_tupla)
+print(type(una_tupla))
+
+#ejercicios dia 1
+
+print(2+3)
+print(3-1)
+print(2*3)
+print(3/2)
+print(3**2)
+print(3%2)
+print(3//2)
+
+#verificando tipos de datos
+
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type('David'))
+print(type([1,2,3,4]))
+print(type({'name':'Andres'}))
+print(type({9.8,6.5,4.6}))
+print(type((9.8,6.8,3.5)))
+
+#formula para la distancia euclidiana
+
+# coordenadas (2,3) y (10,8)
+x1 = 2
+y1 = 3
+x2 = 10
+y2 = 8
+
+distancia = ((x2-x1)**2 + (y2-y1)**2)**0.5
+
+print('La distancia euclidiana entre dos puntos es: %f' %(distancia))
+
+
+
+
+
+
+
+
---
+++ dia2/dia2.py
@@ -0,0 +1,61 @@
+
+
+"""
+# Variables in Python
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+country = 'Finland'
+city = 'Helsinki'
+age = 249
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Asabeneh',
+   'lastname':'Yetayeh',
+   'country':'Finland',
+   'city':'Helsinki'
+   }
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+nombre = input('Cual es tu nombre: ')
+edad = input('Cual es tu edad: ')
+
+print('Nombre: ', nombre)
+print('Edad: ', edad)
+"""
+
+#entero a flotante
+entero = 20
+print('Numero entero: ',entero)
+flotante = float(entero)
+print('Numero flotante: ',flotante)
+
+#flotante a entero
+gravity = 9.81
+print('el valor de la gravedad en entero es: ',int(gravity))
+
+#int to str
+nentero = 10
+print('Numero entero: ',nentero)
+nstr = str(nentero)
+print('Numero entero a texto: ',nstr)
+
+#str a int o float
+nstr2 = '10.6'
+#print('de str a entero: ', int(nstr2))
+print('de str a float: ', float(nstr2))
+
+num_str = '10.6'
+print(int(num_str))
+print('num_int', int(num_str))      # 10
+print('num_float', float(num_str))  # 10.6
