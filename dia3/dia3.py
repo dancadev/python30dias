f5e1ac02dd5d1cd01aad254a47035b44fb909d50
dancadev <dancadev@gmail.com>
2023-01-05 16:20:35 -0500

    avanzando en el dia 3

---
+++ dia3/dia3.py
@@ -0,0 +1,96 @@
+'''
+# OPERADORES
+
+#BOOBLEANOS
+print(True)
+print(False)
+
+#ARITMETICOS
+
+#ENTEROS
+print('Adicion o suma: ', 1+2)
+print('Substracion o resta: ', 2-1)
+print('Multiplicación: ', 2*3)
+print('División: ', 6/2)
+print('Division: ',4/2)
+print('Division: ', 7/2)
+print('Division sin el resto: ', 7//2)
+print('Division sin el resto: ', 7//3)
+print('Modulo MOD', 3%2)
+print('Exponenciación: ', 2**3)
+
+#FLOAT
+
+print('Numero flotante PI: ', 3.14)
+print('Numero flotante  de Gravedad: ', 9.81)
+
+#COMPLEJOS
+
+print('Numero complejo: ', 1+1j)
+print('Multiplicando Numeros Complejos: ',(1+1j)*(1-1j))
+'''
+'''
+#declaracione de variables:
+#se debe declarar la variable primero:
+
+a=3
+b=2
+
+#operaciones artimeticas y asignar resultados a la variable
+
+suma = a+b
+diferencia = a-b
+multiplicacion = a*b
+division = a/b
+mod = a%b
+floorDivision = a//b
+exponencial = a**b
+
+print('El total de la suma es: ',suma)
+print('La diferencia es: ',diferencia)
+print('La multiplicación es: ',multiplicacion)
+print('La division es: ', division)
+print('Mod es:',mod)
+print('Floor Division es:',floorDivision)
+print('Exponencial es: ',exponencial)
+
+'''
+'''
+print('=== Suma,resta,multiplicación, división, modulo ===')
+n1= 3
+n2 = 4
+
+suma = n1+n2
+resta = n2-n1
+multiplicacion =  n1*n2
+division = n2 / n1
+modulo = n2 % n1
+
+print('El total de la suma es: ',suma)
+print('La diferencia es: ',resta)
+print('La multiplicación es: ',multiplicacion)
+print('La division es: ', division)
+print('Mod es:',modulo)
+
+'''
+
+#calculando el area de un circulo
+
+radio = input('Ingrese el valor de Radio: ')
+print('El Radio es: ',radio)
+
+area_circulo = 3.1416 * int(radio) ** 2
+print('El area del circulo es: ',area_circulo)
+
+#calulando el area de un rectangulo:
+
+ancho = input('Ingrese el valor del ancho del rectangulo: ')
+print('Usted ingreso como valor de ANCHO :',ancho)
+alto = input('Ingrese el calor del alto del rectangulo: ')
+print('Usted ingreso el valor de ALTO',alto)
+
+area_rectangulo = float(ancho)*float(alto)
+print('El area del triangulo es: ',area_rectangulo)
+
+
+
