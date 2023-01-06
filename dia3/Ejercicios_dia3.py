38b5fb24c9f30ef3ad0f01b3523dfadb01a122cb
dancadev <dancadev@gmail.com>
2023-01-06 11:47:19 -0500

    se inicio el dia 4

---
+++ dia3/Ejercicios_dia3.py
@@ -0,0 +1,26 @@
+#declara tu edad como una variable entera
+
+edad = 33
+altura = 1.75
+ncomplejo = 3j
+
+#area de un triangulo:
+
+base = input('Por favor ingrese la base del triangulo: ')
+print('usted ingreso: ', base)
+
+altura = input('Ingrese porfavor la altura del triangulo: ')
+print('Usted ingreso como altura: ', altura)
+
+area = (float(base)*float(altura))/2
+print('El area del triangulo es: ', area)
+
+#perimetro del triangulo
+
+ladoa = float(input('Ingrese el valor del lado A: '))
+ladob = float(input('Ingrese el valor del lado B: '))
+ladoc = float(input('Ingrese el valor del lado C: '))
+
+perimetro = ladoa+ladob+ladoc
+
+print('El perimetro del triangulo es: ',perimetro)
