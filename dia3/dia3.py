380d80740ff16a116f184ff98b2bcb0a004edcbb
dancadev <dancadev@gmail.com>
2023-01-06 11:48:07 -0500

    Se inicio el dia 4 strings

--- dia3/dia3.py
+++ dia3/dia3.py
@@ -108,6 +108,7 @@ print('la densidad del liguido es : ', densidad , ' Kg/m^3')
 
 
 '''
+'''
 #operadores de comparacion
 
 print(3>2)
@@ -125,4 +126,35 @@ print(len('leche') == len('carne'))
 print(len('tomate') == len('papa'))
 print(len('python') > len('dragon'))
 
+#comparndo valores de tru o false 
+
+print('True == True', True == True)
+print('True == False', True == False)
+print('False == False', False == False)
+
+'''
+#is, is not ,in,not in
+
+print('1 is 1', 1 is 1)                   # True - because the data values are the same
+print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
+print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
+print('B in Asabeneh', 'B' in 'Asabeneh') # False - there is no uppercase B
+print('coding' in 'coding for all') # True - because coding for all has the word coding
+print('a in an:', 'a' in 'an')      # True
+print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+
+#operadores logicos
+print(3 > 2 and 4 > 3) # True - because both statements are true
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print('True and True: ', True and True)
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statements is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print('True or False:', True or False)
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
 
---
+++ dia4/dia4.py
@@ -0,0 +1,12 @@
+#strings
+
+letra = 'A'
+print(letra)
+print(len(letra))
+
+saludo = 'Hola bienvenido siga asi!!!'
+print(saludo)
+print(len(saludo))
+
+sentencia = "Tambien se puede colocar con comillas dobles"
+print(sentencia)
