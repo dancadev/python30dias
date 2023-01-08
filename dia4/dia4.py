ed2a19349fb95b847c1c566aad6436c9644619eb
dancadev <dancadev@gmail.com>
2023-01-08 11:42:08 -0500

    Strings

--- dia4/dia4.py
+++ dia4/dia4.py
@@ -1,5 +1,6 @@
 #strings
 
+'''
 letra = 'A'
 print(letra)
 print(len(letra))
@@ -10,3 +11,97 @@ print(len(saludo))
 
 sentencia = "Tambien se puede colocar con comillas dobles"
 print(sentencia)
+
+cadena_multilinea = """Esta es una cadena multilinea
+todo aqui se imprime hasta 
+los
+espacios
+""" 
+print(cadena_multilinea)
+
+#concatenacion de cadenas
+
+nombre = 'Johana'
+apellido = 'Enriquez'
+espacio = ' '
+nombre_completo = nombre + espacio + apellido
+print(nombre_completo)
+
+print(len(nombre))
+print(len(apellido))
+print(len(espacio))
+print(len(nombre_completo))
+
+#secuentcia de escape de cadenas
+
+print('Fuerza y \nFe') #salto de linea
+print('Dias\tTopics\tEjercicios') #agregando tab de 4 espacios
+print('Dia 1\t3\t5')
+print('Dia 2\t3\t5')
+print('Dia 3\t3\t5')
+print('Dia 4\t3\t5')
+print('Este es el simbolo de backslash (\\)')
+print('todos los lenguajes de programacion inician con \"Hola mundo\"')
+
+#formato de cadena
+
+# %s %d %f %.number of digitsf
+
+# solamente cadenas
+primer_nombre = 'David'
+primer_apellido = 'Calvache'
+lenguaje = 'Python'
+cadena_formateada = 'Mi nombre es %s %s y estoy estudiando %s'%(primer_nombre,primer_apellido,lenguaje)
+print(cadena_formateada)
+
+#cadenas y numeros
+
+radio = 10
+pi = 3.1416
+area = pi * radio **2
+Cadena_formateada2 = 'el area del circulo con radio %d es %.2f'%(radio,area)
+print(Cadena_formateada2)
+
+librerias_python = ['Django','Flask','NumPy','Matplotlib','Pandas']
+cadformat = 'Estas son algunas de las librerias de python: %s'%(librerias_python)
+print(cadformat)
+
+'''
+'''
+#Nuevo formato de estilo de cadena str.format
+
+nombre = 'Johana'
+apellido = 'Enriquez'
+lenguaje = 'Python'
+cadenafomato1 = '{} {} es mencionada en lenguaje de programacion {}'.format(nombre,apellido,lenguaje)
+print(cadenafomato1)
+
+a = 4 
+b = 3
+
+print('{} + {} = {}'.format(a,b,a+b))
+print('{} - {} = {}'.format(a,b,a-b))
+print('{} * {} = {}'.format(a,b,a*b))
+print('{} / {} = {:.2f}'.format(a,b,a/b))
+print('{} % {} = {}'.format(a,b,a%b))
+print('{} // {} = {}'.format(a,b,a//b))
+print('{} ** {} = {}'.format(a,b,a**b))
+
+#nuevo formato para cadenas y numeros
+
+radio = 10
+pi = 3.1416
+area = pi * radio **2
+cadena_formato2 = 'El area de un circulo con radio {} es {:.2f}'.format(radio,area)
+print(cadena_formato2)
+
+'''
+
+#interpolacion de cadenas / cadenas f
+
+a = 4
+b = 3
+print(f'{a} + {b} = {a+b}')
+
+
+
